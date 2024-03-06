b95e45683e481532f8b6a082d0c6407ae63451df
damian23415 <damian.radomski9908@gmail.com>
2024-03-06 22:59:43 +0100

    Add sign-up form with styles

--- ExpenseTrackPro/ClientApp/src/App.tsx
+++ ExpenseTrackPro/ClientApp/src/App.tsx
@@ -1,12 +1,12 @@
 ﻿import { Routes, Route } from 'react-router-dom'
 
-import SigninForm from './_auth/forms/SigningForm';
+import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import { Home} from './_root/pages';
 
-import './globals.css';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
+import './globals.css';
 
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
                 {/* public routes*/}
                 <Route element={<AuthLayout />}>
                     <Route path="/sign-in" element={<SigninForm />} />
-                    <Route path="/sign-in" element={<SignupForm />} />
+                    <Route path="/sign-up" element={<SignupForm />} />
                 </Route>
 
                 {/* private routes*/}
