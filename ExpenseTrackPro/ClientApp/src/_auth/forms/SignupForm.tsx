3be4054abca79c9afc7e43c26b9473b43e650157
damian23415 <damian.radomski9908@gmail.com>
2024-03-12 00:30:39 +0100

    Add first frontend request to backend with response

--- ExpenseTrackPro/ClientApp/src/_auth/forms/SignupForm.tsx
+++ ExpenseTrackPro/ClientApp/src/_auth/forms/SignupForm.tsx
@@ -1,157 +1,205 @@
-﻿import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Link } from 'react-router-dom'
+﻿import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router-dom";
 
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { Toaster } from "@/components/ui/toaster";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-import { Button } from "@/components/ui/button"
-import { useForm } from "react-hook-form"
-import { SignupValidation } from "@/lib/validation"
-import { Loader } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { SignupValidation } from "@/lib/validation";
+import { Loader } from "lucide-react";
+import { SignUp } from "../api/Helper.js";
+import { useToast } from "@/components/ui/use-toast";
+import { AxiosError, AxiosResponse } from "axios";
+import { ErrorTypes } from "@/constants/errorTypes.js";
 
 const SignupForm = () => {
-    const isLoading = false;
+  const { toast } = useToast();
+  const isLoading = false;
 
-    const form = useForm<z.infer<typeof SignupValidation>>({
-      resolver: zodResolver(SignupValidation),
-      defaultValues: {
-        firstName: '',
-        lastName: '',
-        gender: '',
-        username: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-      },
-    })
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      gender: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
-    // 1. Define your form.
- 
+  // 1. Define your form.
+
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    let parsedErrors: any;
+
+    const response = await SignUp(values)
+      .then((response: AxiosResponse) => {
+        console.log(response);
+        let errorMessage: any;
+
+        if (
+          response.data.errorCode === ErrorTypes.AccountWithEmailAlreadyExist
+        ) {
+          errorMessage = "Account with given email address already exists";
+          console.log(errorMessage);
+          return toast({
+            title: response.data.statusCode,
+            description: errorMessage,
+          });
+        }
+      })
+      .catch((err: AxiosError) => {
+        return toast({
+          title: "Something went wrong",
+        });
+      });
   }
 
-    return (
-        <Form {...form}>
-            <div className="sm:w-420 flex-center flex-col">
-                <img src="/assets/images/logo.svg" alt="logo" />
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="logo" />
 
-                <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new Account</h2>
-                <p className="text-light-3 small-medium md:base-regular mt-2">To use ExpenseTrackPro enter your account details</p>
-            
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new Account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use ExpenseTrackPro enter your account details
+        </p>
 
-                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
-                    <FormField
-                        control={form.control}
-                        name="firstName"
-                        render={({ field }) => (
-                        <FormItem className="flex flex-col">
-                            <FormLabel>First Name</FormLabel>
-                                <FormControl>
-                                    <Input type="text" className="shad-input" {...field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="lastName"
-                        render={({ field }) => (
-                        <FormItem className="flex flex-col">
-                            <FormLabel>Last Name</FormLabel>
-                                <FormControl>
-                                    <Input type="text" className="shad-input" {...field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        render={({ field }) => (
-                        <FormItem className="flex flex-col">
-                            <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input type="text" className="shad-input" {...field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="gender"
-                        render={({ field }) => (
-                        <FormItem className="flex flex-col">
-                            <FormLabel>Gender</FormLabel>
-                                <FormControl>
-                                    <Input type="text" className="shad-input" {...field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                        <FormItem className="flex flex-col">
-                            <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input type="email" className="shad-input" {...field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({ field }) => (
-                        <FormItem className="flex flex-col">
-                            <FormLabel>Password</FormLabel>
-                                <FormControl>
-                                    <Input type="password" className="shad-input" {...field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="confirmPassword"
-                        render={({ field }) => (
-                        <FormItem className="flex flex-col">
-                            <FormLabel>Confirm Password</FormLabel>
-                                <FormControl>
-                                    <Input type="password" className="shad-input" {...field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                        )}
-                    />
-                    <Button type="submit" className="shad-button_primary">
-                        {isLoading ? (
-                            <div className="flex-center gap-2">
-                                <Loader /> Loading...
-                            </div>
-                        ): "Sign up"}
-                    </Button>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Gender</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader /> Loading...
+              </div>
+            ) : (
+              "Sign up"
+            )}
+          </Button>
 
-                    <p className="text-small-regular text-light-3 text-center mt-2">
-                        Already have an account?
-                        <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
-                    </p>
-                </form>
-            </div>
-        </Form>
-    )
-}
+          <p className="text-small-regular text-light-3 text-center mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Log in
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
+};
 
-export default SignupForm
+export default SignupForm;
--- ExpenseTrackPro/ClientApp/src/lib/validation/index.ts
+++ ExpenseTrackPro/ClientApp/src/lib/validation/index.ts
@@ -1,11 +1,20 @@
-import { z } from "zod"
+import { z } from "zod";
 
-export const SignupValidation = z.object({
-    firstName: z.string().min(2, { message: "Too short"}),
-    lastName: z.string().min(2, { message: "Too short"}),
-    gender: z.string().min(2, { message: "Too short"}),
-    username: z.string().min(2, { message: "Too short"}),
+export const SignupValidation = z
+  .object({
+    firstName: z.string().min(2, { message: "Too short" }),
+    lastName: z.string().min(2, { message: "Too short" }),
+    gender: z.string().min(2, { message: "Too short" }),
+    username: z.string().min(2, { message: "Too short" }),
     email: z.string().email(),
-    password: z.string().min(8, { message: "Password must be at least 8 characters."}),
-    confirmPassword: z.string()
-})
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters." }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters." }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords does not match",
+    path: ["confirmPassword"],
+  });
